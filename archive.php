ee9e002faad0982af6678766a79a7461bf3b4bb1
venelinb <venelinborisov5@gmail.com>
2019-12-10 14:30:19 +0200

    Create posts archive template

--- archive.php
+++ archive.php
@@ -14,26 +14,40 @@ get_header();
 	
 		<section id="primary" class="content-area">
 			<main id="main" class="site-main">
-
+				<div class="archive-title-container">
+					<div class="smallis-container">					
+						<?php echo __( 'Archive for ', 'smallis' ) . single_cat_title( '', false ); ?>
+					</div><!-- smallis container -->
+				</div><!-- page title container -->
 			<?php
 			if ( have_posts() ) {
-
-				// Load posts loop.
-				while ( have_posts() ) {
-					the_post();
 ?>
-					<div class="page-title-container">
-						<div class="smallis-container">					
-							<?php echo the_title(); ?>
-						</div><!-- smallis container -->
-					</div><!-- page title container -->
-					<div class="smallis-container">
+				<div class="smallis-container">
+					<div class="smallis-grid-container smallis-posts-container">
 <?php
-						echo the_content();
+					while ( have_posts() ) {
+						the_post();
 ?>
-					</div><!-- smallis container -->
+						<div class="smallis-post">
+							<a href="<? echo get_the_post_permalink(); ?>" title="<?php echo the_title(); ?>">
+								<div class="smallis-post-image-container" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+								</div>
+								<div class="smallis-post-title-container">
+									<?php echo the_title( '<h3>', '</h3>' ); ?>
+								</div>
+								<div class="smallis-post-exceprt-container">	
+<?php
+									echo the_excerpt();
+?>
+								</div>
+								</a>
+						</div><!-- smallis post -->
 <?php				
 				}
+?>
+					</div>
+				</div><!-- smallis container -->	
+<?php				
 			} 
 			
 			?>

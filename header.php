ac87431e37db1695a8cd7e88443595991b7a2a15
venelinb <venelinborisov5@gmail.com>
2019-12-10 10:49:59 +0200

    Setup theme header structure

--- header.php
+++ header.php
@@ -30,4 +30,16 @@
 	<body <?php body_class(); ?>>
 
 		<header>
+
+			<div class="smallis-header-wrap">
+
+				<div class="smallis-site-info">
+
+					<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+				</div><!-- smallis site info -->
+				
+				<div class="smallis-navigation-wrap">
+
+				</div><!-- smallis navigation wrap -->	
+			</div><!-- smallis header wrap-->		
 		</header>

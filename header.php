755254c24f9f3f5c294099d858823e10a3de806d
venelinb <venelinborisov5@gmail.com>
2019-12-10 10:07:39 +0200

    create header.php

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Header
+ *
+ *
+ * @package WordPress
+ * @subpackage Smallis
+ * @since 1.0.0
+ */
+?>
+
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="UTF-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<?php wp_head(); ?>
+
+		<title><?php bloginfo( 'name' ); ?></title>
+
+	</head>
+
+	<body <?php body_class(); ?>>
+
+		<header>
+		</header>

68c07a6958d524f639b9c4daa988d081d43a2920
venelinb <venelinborisov5@gmail.com>
2019-12-10 16:02:16 +0200

    Create single post template

---
+++ single.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The single template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Smallis
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+	
+		<section id="primary" class="content-area">
+			<main id="main" class="site-main">
+
+			<?php
+			if ( have_posts() ) {
+
+				// Load posts loop.
+				while ( have_posts() ) {
+					the_post();
+
+					if( has_post_thumbnail() ) {
+						get_template_part( 'template-parts/post-title', 'thumbnail' );
+					} else {
+						get_template_part( 'template-parts/post-title' );
+					}
+?>
+					
+					<div class="smallis-container">
+						<div class="smallis-grid-container smallis-post-container">
+							<div class="post-content">
+<?php
+								echo the_content();
+?>
+							</div>
+							<div class="post-sidebar justify-end">
+<?php							
+								dynamic_sidebar( 'main-sidebar' );
+?>
+							</div>
+						</div>
+					</div><!-- smallis container -->
+<?php				
+				}
+			} 
+			
+			?>
+
+			</main><!-- .site-main -->
+		</section><!-- .content-area -->
+		
+<?php
+get_footer();

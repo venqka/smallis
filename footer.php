4fc0a80c5891a0bda00b1e887e7267d42716a107
venelinb <venelinborisov5@gmail.com>
2019-12-10 10:55:36 +0200

    Create footer structure

--- footer.php
+++ footer.php
@@ -11,7 +11,32 @@
 
 
 		<footer class="container">
+			<div class="smallis-footer-columns">
 
+				<div class="smallis-footer-column">
+					Here be widgets
+				</div><!-- smallis footer column -->
+				
+				<div class="smallis-footer-column">
+					Here be widgets
+				</div><!-- smallis footer column -->
+
+				<div class="smallis-footer-column">
+					Here be widgets
+				</div><!-- smallis footer column -->
+
+			</div><!-- smallis footer columns -->
+			<div class="smallis-footer-copyright">
+
+				<div class="smallis-footer-copyright-left">
+					<p>Prowdly Powered by Wordpress</p>
+				</div><!-- smallis footer copyright left-->
+				
+				<div class="smallis-footer-copyright-right">
+					<p>Here be copyright text</p>
+				</div><!-- smallis footer copyright right-->
+
+			</div><!-- smallis footer copyright -->	
 		</footer>
 
 		<?php wp_footer(); ?>

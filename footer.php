74f6a73bc3ed44b19a3f80d67a725ed6a839cf17
venelinb <venelinborisov5@gmail.com>
2019-12-10 10:07:24 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Footer
+ *
+ *
+ * @package WordPress
+ * @subpackage Smallis
+ * @since 1.0.0
+ */
+?>
+
+
+		<footer class="container">
+
+		</footer>
+
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html>

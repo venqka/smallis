adedd06634bea6c1d8f8082438e228ec1adc7e94
venelinb <venelinborisov5@gmail.com>
2019-12-10 15:27:22 +0200

    Add single post title template parts

---
+++ template-parts/post-title-thumbnail.php
@@ -0,0 +1,8 @@
+<div class="smallis-container">
+	<div class="post-image-container" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+		
+		<div class="post-title-container">			
+			<?php echo the_title(); ?>
+		</div>
+	</div><!-- post image container -->
+</div><!-- smallis container -->
---
+++ template-parts/post-title.php
@@ -0,0 +1,5 @@
+<div class="post-title-container">
+	<div class="smallis-container">					
+		<?php echo the_title(); ?>
+	</div><!-- smallis container -->
+</div><!-- page title container -->

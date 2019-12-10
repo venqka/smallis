c77cba5460168734dedf0b22d7f87e98ab3cbe11
venelinb <venelinborisov5@gmail.com>
2019-12-10 14:53:42 +0200

    Register smallis sidebars

--- functions.php
+++ functions.php
@@ -39,3 +39,54 @@ if( !function_exists( 'smallis_navigation' ) ) {
 	add_action( 'init', 'smallis_navigation' );
 
 }
+
+if( !function_exists( 'smallis_sidebars' ) ) {
+
+	function smallis_sidebars() {
+
+		$smallis_main_sidebar_arrgs = array(
+			'name'          => __( 'Main Sidebar', 'smallis' ),
+        	'id'            => 'main-sidebar',
+        	'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'smallis' ),
+        	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        	'after_widget'  => '</li>',
+        	'before_title'  => '<h2 class="widgettitle">',
+        	'after_title'   => '</h2>',
+		);
+		register_sidebar( $smallis_main_sidebar_arrgs );
+
+		$smallis_footer1_sidebar_arrgs = array(
+			'name'          => __( 'Footer Column 1', 'smallis' ),
+        	'id'            => 'footer1-sidebar',
+        	'description'   => __( 'Widgets in this area will be shown in the first footer column.', 'smallis' ),
+        	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        	'after_widget'  => '</li>',
+        	'before_title'  => '<h2 class="widgettitle">',
+        	'after_title'   => '</h2>',
+		);
+		register_sidebar( $smallis_footer1_sidebar_arrgs );
+
+		$smallis_footer2_sidebar_arrgs = array(
+			'name'          => __( 'Footer Column 2', 'smallis' ),
+        	'id'            => 'footer2-sidebar',
+        	'description'   => __( 'Widgets in this area will be shown in the first footer column.', 'smallis' ),
+        	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        	'after_widget'  => '</li>',
+        	'before_title'  => '<h2 class="widgettitle">',
+        	'after_title'   => '</h2>',
+		);
+		register_sidebar( $smallis_footer2_sidebar_arrgs );
+
+		$smallis_footer3_sidebar_arrgs = array(
+			'name'          => __( 'Footer Column 3', 'smallis' ),
+        	'id'            => 'footer3-sidebar',
+        	'description'   => __( 'Widgets in this area will be shown in the first footer column.', 'smallis' ),
+        	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        	'after_widget'  => '</li>',
+        	'before_title'  => '<h2 class="widgettitle">',
+        	'after_title'   => '</h2>',
+		);
+		register_sidebar( $smallis_footer3_sidebar_arrgs );
+	}
+	add_action( 'widgets_init', 'smallis_sidebars' );
+}

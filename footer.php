24e4727aca91844170528e9c8801c3f5bfbac80b
venelinb <venelinborisov5@gmail.com>
2019-12-10 11:09:08 +0200

    Add smallis-container to footer

--- footer.php
+++ footer.php
@@ -10,33 +10,35 @@
 ?>
 
 
-		<footer class="container">
-			<div class="smallis-footer-columns">
+		<footer>
+			<div class="smallis-container">
+				<div class="smallis-footer-columns">
 
-				<div class="smallis-footer-column">
-					Here be widgets
-				</div><!-- smallis footer column -->
-				
-				<div class="smallis-footer-column">
-					Here be widgets
-				</div><!-- smallis footer column -->
+					<div class="smallis-footer-column">
+						Here be widgets
+					</div><!-- smallis footer column -->
+					
+					<div class="smallis-footer-column">
+						Here be widgets
+					</div><!-- smallis footer column -->
 
-				<div class="smallis-footer-column">
-					Here be widgets
-				</div><!-- smallis footer column -->
+					<div class="smallis-footer-column">
+						Here be widgets
+					</div><!-- smallis footer column -->
 
-			</div><!-- smallis footer columns -->
-			<div class="smallis-footer-copyright">
+				</div><!-- smallis footer columns -->
+				<div class="smallis-footer-copyright">
 
-				<div class="smallis-footer-copyright-left">
-					<p>Prowdly Powered by Wordpress</p>
-				</div><!-- smallis footer copyright left-->
-				
-				<div class="smallis-footer-copyright-right">
-					<p>Here be copyright text</p>
-				</div><!-- smallis footer copyright right-->
+					<div class="smallis-footer-copyright-left">
+						<p>Prowdly Powered by Wordpress</p>
+					</div><!-- smallis footer copyright left-->
+					
+					<div class="smallis-footer-copyright-right">
+						<p>Here be copyright text</p>
+					</div><!-- smallis footer copyright right-->
 
-			</div><!-- smallis footer copyright -->	
+				</div><!-- smallis footer copyright -->	
+			</div><!-- smallis container -->
 		</footer>
 
 		<?php wp_footer(); ?>

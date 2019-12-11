b1d55a04d2b5c282b224b60136295214b7ef9b67
venelinb <venelinborisov5@gmail.com>
2019-12-11 09:22:07 +0200

    Create front page template

---
+++ front-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The front page template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Smallis
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+		<section id="primary" class="content-area">
+			<main id="main" class="site-main">
+
+			<?php
+			if ( have_posts() ) {
+
+				// Load posts loop.
+				while ( have_posts() ) {
+					the_post();
+?>
+					<div class="smallis-container">
+<?php
+						echo the_content();
+?>
+					</div><!-- smallis container -->
+<?php				
+				}
+			} 
+			
+			?>
+
+			</main><!-- .site-main -->
+		</section><!-- .content-area -->
+		
+<?php
+get_footer();

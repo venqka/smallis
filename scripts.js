db4c5dce7b7ea8ea6b3c2474de0a6fe797bcf091
venelinb <venelinborisov5@gmail.com>
2019-12-12 10:07:49 +0200

    Toggle mobile menu

--- scripts.js
+++ scripts.js
@@ -2,8 +2,17 @@ jQuery(document).ready(function(){
 
 	//Toggle desktop submenu
 
-	jQuery('.smallis-navigation-wrap ul li').on('hover', function(){
+	jQuery('.smallis-desktop-menu ul li').on('hover', function(){
 		jQuery(this).children('ul').toggle();
 	});
 
+	//Toggle mobile menu
+	jQuery('.smallis-mobile-menu .smallis-menu-toggle').click(function(){
+		jQuery('.smallis-mobile-menu .smallis-navigation-wrap').toggle();
+	});
+
+	jQuery('.smallis-mobile-menu ul .menu-item-has-children > a').on('click', function(a){
+		a.preventDefault();
+		jQuery(this).next().toggle();
+	});
 });
